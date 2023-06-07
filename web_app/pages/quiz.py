83e4eb49a539beb9a3762d3bd146fd3ea5a06de9
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-06-07 20:31:02 +0800

    feat: added scores style: changed code structure to make it readable

--- web_app/pages/quiz.py
+++ web_app/pages/quiz.py
@@ -1,6 +1,10 @@
 """
 The following acts as the main page of the web app.
 For future additions, Streamlit identifies Markdown text.
+
+To whoever's going to document this, someone read PEP.
+Thanks. -Omi
+
 """
 
 # Initial configurations
@@ -14,16 +18,30 @@ st.set_page_config(initial_sidebar_state="collapsed")
 if 'Current Page' not in st.session_state:
     st.session_state['Current Page'] = 0
 
+# To pretty much allow variables to be tracked in
+# debug and also to allow it to change on the fly.
+if 'Scores' not in st.session_state:
+    st.session_state['Scores'] = [
+        0,  #m_app
+        0,  #m_speak
+        0,  #m_phys
+        0,  #m_mental
+        0,  #m_conf
+        0,  #m_ideas
+        0,  #m_comm
+        0   #m_perf
+    ]
+
 #TO DO: Initialize global variables here (?)
 
-m_app = 3
-m_speak = 3
-m_phys = 3
-m_mental = 3
-m_conf = 3
-m_ideas = 3
-m_comm = 3
-m_perf = 3
+# m_app = 3
+# m_speak = 3
+# m_phys = 3
+# m_mental = 3
+# m_conf = 3
+# m_ideas = 3
+# m_comm = 3
+# m_perf = 3
 
 @st.cache_resource
 def load_model():
@@ -70,6 +88,7 @@ def predict_answers(m_app, m_speak, m_phys, m_mental, m_conf, m_ideas, m_comm, m
 # STUDENT PERFORMANCE RATING  = 7
 #============================
 
+
 def next():
     st.session_state['Current Page'] += 1
     
@@ -93,13 +112,19 @@ if st.session_state['Current Page'] == 0:
     with app_col1:
         # item 1
         st.write("What outfit do you see yourself wearing in:")
-        m_app_choice_1 = st.radio(label="A Formal Interview, where the company's employees usually wear semi-formal attire", options=["Semi-Formal Wear", "Business Casual Wear", "Formal Wear"]) # Best answers (from best to worst): Formal Wear, Semi-Formal Wear, Business Casual
+        m_app_choice_1 = st.radio(
+            label="A Formal Interview, where the company's employees usually wear semi-formal attire",
+            options=["Semi-Formal Wear", "Business Casual Wear", "Formal Wear"]) # Best answers (from best to worst): Formal Wear, Semi-Formal Wear, Business Casual
     
         # item 2
-        m_app_choice_2 = st.radio(label='In a Casual Interview at a coffee shop, that your interviewer will wear something Smart Casual', options=["Business Casual Wear", "Casual Wear", "Formal Wear"]) # Best to Worst Answers: Business Casual Wear, Casual Wear, Formal Wear
+        m_app_choice_2 = st.radio(
+            label='In a Casual Interview at a coffee shop, that your interviewer will wear something Smart Casual',
+            options=["Business Casual Wear", "Casual Wear", "Formal Wear"]) # Best to Worst Answers: Business Casual Wear, Casual Wear, Formal Wear
 
         # item 3
-        m_app_choice_3 = st.radio(label="In a Group Interview, where the company's dress code is casual", options=["Casual Wear", "Semi-Formal Wear", "Business Casual Wear"]) # Best to Worst Answers: Business Casual Wear, Semi-Formal Wear, Casual Wear
+        m_app_choice_3 = st.radio(
+            label="In a Group Interview, where the company's dress code is casual",
+            options=["Casual Wear", "Semi-Formal Wear", "Business Casual Wear"]) # Best to Worst Answers: Business Casual Wear, Semi-Formal Wear, Casual Wear
 
     with app_col2:
         img1 = st.image("files/formal_clothing.png", width = 200, caption="Formal Wear")
@@ -109,6 +134,35 @@ if st.session_state['Current Page'] == 0:
         img3 = st.image("files/businesscasual_clothing.png", width = 200, caption="Business Casual Wear")
         img4 = st.image("files/casual_clothing.png", width = 200, caption="Casual Wear")
 
+    # God abandoned me this way.
+    st.session_state['Scores'][0] = 0
+    # First Answer
+    match m_app_choice_1: 
+        case "Semi-Formal Wear":
+            st.session_state['Scores'][0] += 4
+        case "Business Casual Wear":
+            st.session_state['Scores'][0] += 3
+        case "Formal Wear":
+            st.session_state['Scores'][0] += 5
+    # Second Answer
+    match m_app_choice_2:
+        case "Formal Wear":
+            st.session_state['Scores'][0] += 4
+        case "Business Casual Wear":
+            st.session_state['Scores'][0] += 5
+        case "Casual Wear":
+            st.session_state['Scores'][0] += 3
+    # Third Answer
+    match m_app_choice_3:
+        case "Semi-Formal Wear":
+            st.session_state['Scores'][0] += 4
+        case "Business Casual Wear":
+            st.session_state['Scores'][0] += 5
+        case "Casual Wear":
+            st.session_state['Scores'][0] += 3
+    # Average the answers
+    st.session_state['Scores'][0] /= 3
+    
     next_button = st.button(label="Next",on_click=next)
     
     # TO DO: insert credits for images used here  
@@ -117,21 +171,65 @@ if st.session_state['Current Page'] == 0:
 elif st.session_state['Current Page'] == 1:
     st.write("MANNER OF SPEAKING")
     # item 1
-    m_speak_choice_1 = st.radio(label="You are addressing a customer complaint as a customer service representative, choose the most appropriate response", options=["Sorry for the inconvenience caused. We'll do our best to resolve the issue shortly.", "It's not our fault. You should have read the terms and conditions more carefully.", "That's not my department. You need to speak with someone else."]) # best to worst (by index): 0, 1, 2
+    m_speak_choice_1 = st.radio(
+        label="You are addressing a customer complaint as a customer service representative, choose the most appropriate response",
+        options=[
+            "Sorry for the inconvenience caused. We'll do our best to resolve the issue shortly.",
+            "It's not our fault. You should have read the terms and conditions more carefully.",
+            "That's not my department. You need to speak with someone else."
+            ]) # best to worst (by index): 0, 1, 2
     
     # item 2
     st.write("Scenario 2: {insert scenario here}, what would you say?")
-    m_speak_choice_2 = st.radio(label='You are in a group discussion, and a team member continuously interrupts and dominates the conversation. How would you respond?', options=["Say: Excuse me, but you're talking too much. Let others have a chance to speak.",  "Say: I appreciate your enthusiasm, but it would be beneficial to hear other perspectives as well.", "(Let them continue what they're saying)"]) # best to worst (by index): 1, 0, 2
+    m_speak_choice_2 = st.radio(
+        label='You are in a group discussion, and a team member continuously interrupts and dominates the conversation. How would you respond?',
+        options=[
+            "Say: Excuse me, but you're talking too much. Let others have a chance to speak.",
+            "Say: I appreciate your enthusiasm, but it would be beneficial to hear other perspectives as well.",
+            "(Let them continue what they're saying)"
+            ]) # best to worst (by index): 1, 0, 2
     
     # item 3
-    m_speak_choice_3 = st.radio(label='You are in a team meeting and a colleague presents an idea that you strongly disagree with. What would you say?', options=["I think that idea won't work because...", "I appreciate your perspective, although I have different viewpoint on this because...", "I think your idea is completely off-track, the project's goal is to..."]) # best to worst (by index): 1, 0, 2
+    m_speak_choice_3 = st.radio(
+        label='You are in a team meeting and a colleague presents an idea that you strongly disagree with. What would you say?',
+        options=[
+            "I think that idea won't work because...",
+            "I appreciate your perspective, although I have different viewpoint on this because...",
+            "I think your idea is completely off-track, the project's goal is to..."
+            ]) # best to worst (by index): 1, 0, 2
 
-    m_speak = 1 + 1 # final m_speak value, placeholder value before making proper equation for it
+    # God abandoned me this way.
+    st.session_state['Scores'][1] = 0
+    # First Answer
+    match m_speak_choice_1: 
+        case "Sorry for the inconvenience caused. We'll do our best to resolve the issue shortly.":
+            st.session_state['Scores'][1] += 5
+        case "It's not our fault. You should have read the terms and conditions more carefully.":
+            st.session_state['Scores'][1] += 4
+        case "That's not my department. You need to speak with someone else.":
+            st.session_state['Scores'][1] += 3
+    # Second Answer
+    match m_speak_choice_2:
+        case "Say: Excuse me, but you're talking too much. Let others have a chance to speak.":
+            st.session_state['Scores'][1] += 4
+        case "Say: I appreciate your enthusiasm, but it would be beneficial to hear other perspectives as well.":
+            st.session_state['Scores'][1] += 5
+        case "(Let them continue what they're saying)":
+            st.session_state['Scores'][1] += 3
+    # Third Answer
+    match m_speak_choice_3:
+        case "I think that idea won't work because...":
+            st.session_state['Scores'][1] += 4
+        case "I appreciate your perspective, although I have different viewpoint on this because...":
+            st.session_state['Scores'][1] += 5
+        case "I think your idea is completely off-track, the project's goal is to...":
+            st.session_state['Scores'][1] += 3
+    # Average the answers
+    st.session_state['Scores'][1] /= 3
 
     next_button = st.button(label="Next",on_click=next)
     back_button = st.button(label="Back",on_click=prev)
 
-    
 # PHYSICAL CONDITION
 elif st.session_state['Current Page'] == 2:
     st.write("PHYSICAL CONDITION")
@@ -142,8 +240,7 @@ elif st.session_state['Current Page'] == 2:
 
     next_button = st.button(label="Next",on_click=next)
     back_button = st.button(label="Back",on_click=prev)
-
-    
+ 
 # MENTAL ALERTNESS
 elif st.session_state['Current Page'] == 3:
     st.write("MENTAL ALERTNESS")
@@ -166,65 +263,119 @@ elif st.session_state['Current Page'] == 3:
     back_button = st.button(label="Back",on_click=prev)
 
     # st.write("Questions based on https://www.jobtestprep.com/thurstone-test-mental-alertness") # just realized this would also reveal the answers KEK
-
-    
+ 
 # SELF CONFIDENCE
 elif st.session_state['Current Page'] == 4:
     st.write("SELF CONFIDENCE")
     
     # item
     st.write("How confident do you see yourself?")
-    m_conf = st.select_slider(label = "Slide to the prompt that best describes you", options=("I wish I am", "Somewhat", "Pretty much!", "Definitely!"))
+    m_conf = st.select_slider(
+        label = "Slide to the prompt that best describes you", 
+        options = (
+            "I wish I am", 
+            "Somewhat", 
+            "Pretty much!", 
+            "Definitely!"
+            )
+        )
 
     next_button = st.button(label="Next",on_click=next)
     back_button = st.button(label="Back",on_click=prev)
 
-    
 # ABILITY TO PRESENT IDEAS
 elif st.session_state['Current Page'] == 5:
     st.write("ABILITY TO PRESENT IDEAS")
 
     st.write("Which of the following is an essential component of a well-structured presentation?")
-    m_ideas_choice_1 = st.radio(label="Choices", options = ["Clear introduction", "Engaging visual aids", "Concise conclusion", "All of the above"]) # Answer: All of the Above
+    m_ideas_choice_1 = st.radio(
+        label="Choices", 
+        options = [
+            "Clear introduction", 
+            "Engaging visual aids", 
+            "Concise conclusion", 
+            "All of the above"
+            ]) # Answer: All of the Above
 
     st.write("What is one of the recommended ways to maintain audience engagement during a presentation?")
-    m_ideas_choice_2 = st.radio(label="Choices", options = ["Use interactive activities", "Incorporate storytelling techniques", "Be controversial with your ideas", "None of the above"]) # Answer: "Incorporate storytelling techniques"
+    m_ideas_choice_2 = st.radio(
+        label="Choices",
+        options = [
+            "Use interactive activities", 
+            "Incorporate storytelling techniques", 
+            "Be controversial with your ideas", 
+            "None of the above"
+            ]) # Answer: "Incorporate storytelling techniques"
 
     st.write("You notice that some audience members appear disengaged during your presentation. What should you do?")
-    m_ideas_choice_3 = st.radio(label="Choices", options = ["Increase your volume and speak louder to make them hear you", "Use more complex technical terms to grab their attention", "Pause and ask a question to encourage participation", "Ignore them and continue with your presentation for those who are actually listening"]) # Answer: Pause and ask a question to encourage participation
+    m_ideas_choice_3 = st.radio(
+        label = "Choices", 
+        options = [
+            "Increase your volume and speak louder to make them hear you", 
+            "Use more complex technical terms to grab their attention", 
+            "Pause and ask a question to encourage participation", 
+            "Ignore them and continue with your presentation for those who are actually listening"
+            ]) # Answer: Pause and ask a question to encourage participation
 
     m_ideas = 1 + 1 #temp values muna, will polish later
 
     next_button = st.button(label="Next",on_click=next)
     back_button = st.button(label="Back",on_click=prev)
 
-
 # COMMUNICATION SKILLS
 elif st.session_state['Current Page'] == 6:
     st.write("COMMUNICATION SKILLS")
 
-    m_comm_choice_1 = st.radio(label="Which of the following is an essential component of effective communication?", options=["Active listening", "Speaking slowly", "Superior vocabulary", "Using complex vocabulary"]) # Answer: Active Listening
+    m_comm_choice_1 = st.radio(
+        label = "Which of the following is an essential component of effective communication?", 
+        options = [
+            "Active listening", 
+            "Speaking slowly", 
+            "Superior vocabulary", 
+            "Using complex vocabulary"
+            ]) # Answer: Active Listening
 
-    m_comm_choice_2 = st.radio(label="How would you handle a situation where you receive an email with unclear instructions?", options=["Ignore the email and wait for clarification.", "Reply with your best guess.", "Seek clarification by asking specific questions.", "Reply with any response to buy time."]) # Answer: Seek clarification by asking specific questions.
+    m_comm_choice_2 = st.radio(
+        label = "How would you handle a situation where you receive an email with unclear instructions?", 
+        options = [
+            "Ignore the email and wait for clarification.", 
+            "Reply with your best guess.", 
+            "Seek clarification by asking specific questions.", 
+            "Reply with any response to buy time."
+            ]) # Answer: Seek clarification by asking specific questions.
 
-    m_comm_choice_3 = st.radio(label="How would you handle a disagreement with a colleague during a team meeting?", options=["Assert your opinion.", "Keep listening and avoid conflict.", "Listen to their perspective and express your viewpoint respectfully.", "Tell the issue to a supervisor."]) # Answer:  "Listen to their perspective and express your viewpoint respectfully."
+    m_comm_choice_3 = st.radio(
+        label = "How would you handle a disagreement with a colleague during a team meeting?", 
+        options = [
+            "Assert your opinion.", 
+            "Keep listening and avoid conflict.", 
+            "Listen to their perspective and express your viewpoint respectfully.", 
+            "Tell the issue to a supervisor."
+            ]) # Answer:  "Listen to their perspective and express your viewpoint respectfully."
 
     m_comm = 1 + 1
 
     next_button = st.button(label="Next",on_click=next)
     back_button = st.button(label="Back",on_click=prev)
 
-
 # STUDENT PERFORMANCE RATING
 elif st.session_state['Current Page'] == 7:
     st.write("STUDENT PERFORMANCE RATING")
 
     st.write("According to your University/School Record, what's your current GWA?")
-    m_phys = st.select_slider(label = "Slide to the range that fits you", options=("3.00-2.50", "2.50-1.50", "1.50-1.00"))
+    m_phys = st.select_slider(
+        label = "Slide to the range that fits you", 
+        options = (
+            "3.00-2.50", 
+            "2.50-1.50", 
+            "1.50-1.00"
+            )
+        )
 
     back_button = st.button(label="Back",on_click=prev)
     submit_button = st.button(label="Submit", on_click=next)
 
+# RESULTS
 elif st.session_state['Current Page'] == 8:
     st.write("YOUR RESULTS:")
     # predict here

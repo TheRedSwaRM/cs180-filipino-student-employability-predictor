62e644bc2b9e359b1f0b4fbc94bb9c4b40b098c6
TheRedSwaRM <rsbalatibat@up.edu.ph>
2023-06-11 19:36:42 +0800

    chore: check if changing path works

--- web_app/pages/quiz.py
+++ web_app/pages/quiz.py
@@ -53,7 +53,7 @@ def load_model():
     Returns:
         _scikit_model_: A saved model from previous training tests.
     """
-    return pickle.load(open('mlp_model.sav', 'rb'))
+    return pickle.load(open(r'../web_app/files/mlp_model.sav', 'rb'))
 
 @st.cache_resource
 def load_scaler():
@@ -62,7 +62,7 @@ def load_scaler():
     Returns:
         _scikit_model_: A saved model from previous training tests.
     """
-    return pickle.load(open('best_scaler.sav', 'rb'))
+    return pickle.load(open(r'../web_app/files/best_scaler.sav', 'rb'))
 
 
 loaded_model = load_model()
